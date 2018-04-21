691cb0e10fac4300ab52a00ca55ae52cef253aaf
Julien Cappiello <contact@kendozone.com>
2018-04-21 18:25:18 +0200

    remove auth

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::group(['middleware' => ['auth'], 'prefix' => 'admin', 'as' => 'admin.'], function () {
-    Route::get('companies', 'CompaniesController@index')->name('companies.index');
-});
+Route::get('companies', 'CompaniesController@index')->name('companies.index');
